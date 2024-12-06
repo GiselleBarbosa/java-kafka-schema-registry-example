f2261413bc79dc81b17859023f31f76467f6d071
GiselleBarbosa <g88barbosa@gmail.com>
2024-12-06 08:02:16 -0300

    feat: consumindo mensagens de particao especifica no Kafka

--- src/main/kotlin/br/com/barbosa/example/consumer/PessoaConsumerImpl.kt
+++ src/main/kotlin/br/com/barbosa/example/consumer/PessoaConsumerImpl.kt
@@ -2,7 +2,10 @@ package br.com.barbosa.example.consumer;
 
 import br.com.barbosa.example.entity.Pessoa;
 import br.com.barbosa.example.entity.PessoaDTO;
-import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.annotation.PartitionOffset
+import org.springframework.kafka.annotation.TopicPartition
+
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 import kotlin.concurrent.thread
@@ -10,12 +13,17 @@ import kotlin.concurrent.thread
 @Component
 public class PessoaConsumerImpl {
 
-    @KafkaListener(topics = ["Pessoa"], groupId = "pessoa-consumer")
+    @KafkaListener(
+        id = "pessoa-consumer",
+        topicPartitions = [TopicPartition(
+            topic = "Pessoa",
+            partitions = ["0"],
+            partitionOffsets = arrayOf(PartitionOffset(partition = "*", initialOffset = "0"))
+        )],
+    )
 
     fun consume(@Payload pessoaDTO: PessoaDTO) {
-
-        Thread.sleep(15000)
-
+        // Thread.sleep(15000)
         val pessoa = Pessoa(pessoaDTO.getNome().toString(), pessoaDTO.getSobrenome().toString())
         println("Pessoa recebida com sucesso: " + pessoa.toString())
     }
